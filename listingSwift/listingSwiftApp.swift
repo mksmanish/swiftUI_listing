258e2f99f56688b893a029d587d2e3d3ec98f2aa
manish <manishk@winjit.com>
2021-05-30 15:07:30 +0530

    Initial Commit

---
+++ listingSwift/listingSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  listingSwiftApp.swift
+//  listingSwift
+//
+//  Created by manish on 30/05/21.
+//
+
+import SwiftUI
+
+@main
+struct listingSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
